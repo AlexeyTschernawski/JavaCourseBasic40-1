37378925cbfa2bd6212ba772bcb0b988089bde19
allex <allexxem@gmail.com>
2024-01-18 16:14:55 +0100

    Hallo

---
+++ src/homeworks/test.java
@@ -0,0 +1,14 @@
+package homeworks;
+
+public class test {
+    public static void main(String[] args) {
+
+        int a = 33;
+        int b = 5;
+        int c = a % b;
+        int d = 22 % 4;
+
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ src/lessons/Lesson3.java
@@ -0,0 +1,8 @@
+package lessons;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+
+    }
+}
